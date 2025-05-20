0a86e3c9f8ac820b18f0dd9493709054fef616bf
Victor Almeida <victor.almeida@polimonitor.com>
2025-05-20 19:33:48 +0100

    initial commit

---
+++ services/gcp.py
@@ -0,0 +1,18 @@
+from google.cloud import secretmanager
+import os
+
+PATH = os.get('PATH')
+
+def get_secret() -> str:
+    """
+    Retrieve secret from GCP Secret Manager
+    """
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = PATH
+    secret_client = secretmanager.SecretManagerServiceClient()
+    name = secret_client.secret_version_path(
+        project="733286937483",
+        secret="ai-agent-ai-key",
+        secret_version="latest"
+    )
+    response = secret_client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
